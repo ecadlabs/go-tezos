87fa9052ecbf62bdd00cff65e12a3514263d9440
Julius Volz <julius.volz@gmail.com>
2018-10-27 19:24:44 +0200

    Added contract/delegate balance getters and table-based tests

--- network.go
+++ network.go
@@ -6,20 +6,32 @@ import (
 	"path"
 )
 
-// NetworkService is an interface for retrieving network stats from the tezos RPC api
-// https://tezos.gitlab.io/betanet/api/rpc.html#get-network-stat
+// NetworkService is an interface for retrieving network stats from the Tezos RPC API.
 type NetworkService interface {
+	// GetStats implements https://tezos.gitlab.io/betanet/api/rpc.html#get-network-stat.
 	GetStats(context.Context) (*NetworkStats, error)
+	// GetConnections implements http://tezos.gitlab.io/mainnet/api/rpc.html#get-network-connections.
 	GetConnections(context.Context) ([]NetworkConnection, error)
 }
 
-// NetworkServiceOp handles communication with the `/network` related tezos RPC methods
-type NetworkServiceOp struct {
-	client *Client
+// ContractService is an interface for retrieving contract-related information from the Tezos RPC API.
+type ContractService interface {
+	// GetDelegateBalance implements http://tezos.gitlab.io/mainnet/api/rpc.html#get-block-id-context-delegates-pkh-balance.
+	GetDelegateBalance(ctx context.Context, chainID string, blockID string, pkh string) (string, error)
+	// GetContractBalance implements http://tezos.gitlab.io/mainnet/api/rpc.html#get-block-id-context-contracts-contract-id-balance.
+	GetContractBalance(ctx context.Context, chainID string, blockID string, contractID string) (string, error)
 }
 
+// Service implements fetching of information from Tezos nodes via JSON.
+type Service struct {
+	client *RPCClient
+}
+
+var _ NetworkService = &Service{}
+var _ ContractService = &Service{}
+
 // GetStats returns current network stats https://tezos.gitlab.io/betanet/api/rpc.html#get-network-stat
-func (s *NetworkServiceOp) GetStats(ctx context.Context) (*NetworkStats, error) {
+func (s *Service) GetStats(ctx context.Context) (*NetworkStats, error) {
 	url := *s.client.BaseURL
 	url.Path = path.Join(url.Path, "/network/stat")
 
@@ -30,7 +42,7 @@ func (s *NetworkServiceOp) GetStats(ctx context.Context) (*NetworkStats, error) 
 
 	stats := new(NetworkStats)
 	//TODO make use of response, only works for GETs
-	_, err = s.client.Do(ctx, req, stats)
+	err = s.client.Get(ctx, req, stats)
 	if err != nil {
 		return nil, err
 	}
@@ -38,7 +50,7 @@ func (s *NetworkServiceOp) GetStats(ctx context.Context) (*NetworkStats, error) 
 }
 
 // GetConnections returns all network connections http://tezos.gitlab.io/mainnet/api/rpc.html#get-network-connections
-func (s *NetworkServiceOp) GetConnections(ctx context.Context) ([]NetworkConnection, error) {
+func (s *Service) GetConnections(ctx context.Context) ([]NetworkConnection, error) {
 	url := *s.client.BaseURL
 	url.Path = path.Join(url.Path, "/network/connections")
 
@@ -48,16 +60,50 @@ func (s *NetworkServiceOp) GetConnections(ctx context.Context) ([]NetworkConnect
 	}
 
 	conns := []NetworkConnection{}
-	_, err = s.client.Do(ctx, req, &conns)
+	err = s.client.Get(ctx, req, &conns)
 	if err != nil {
 		return nil, err
 	}
 	return conns, err
 }
 
-var _ NetworkService = &NetworkServiceOp{}
+// GetDelegateBalance returns a delegate's balance http://tezos.gitlab.io/mainnet/api/rpc.html#get-block-id-context-delegates-pkh-balance
+func (s *Service) GetDelegateBalance(ctx context.Context, chainID string, blockID string, pkh string) (string, error) {
+	url := *s.client.BaseURL
+	url.Path = path.Join(url.Path, "chains", chainID, "blocks", blockID, "context/delegates", pkh, "balance")
 
-// NetworkStats contains Global network bandwidth totals and usage in B/s.
+	req, err := s.client.NewRequest(ctx, http.MethodGet, url.String(), nil)
+	if err != nil {
+		return "", err
+	}
+
+	var balance string
+	err = s.client.Get(ctx, req, &balance)
+	if err != nil {
+		return "", err
+	}
+	return balance, err
+}
+
+// GetContractBalance returns a contract's balance http://tezos.gitlab.io/mainnet/api/rpc.html#get-block-id-context-contracts-contract-id-balance
+func (s *Service) GetContractBalance(ctx context.Context, chainID string, blockID string, contractID string) (string, error) {
+	url := *s.client.BaseURL
+	url.Path = path.Join(url.Path, "chains", chainID, "blocks", blockID, "context/contracts", contractID, "balance")
+
+	req, err := s.client.NewRequest(ctx, http.MethodGet, url.String(), nil)
+	if err != nil {
+		return "", err
+	}
+
+	var balance string
+	err = s.client.Get(ctx, req, &balance)
+	if err != nil {
+		return "", err
+	}
+	return balance, err
+}
+
+// NetworkStats models global network bandwidth totals and usage in B/s.
 type NetworkStats struct {
 	TotalBytesSent int64 `json:"total_sent,string"`
 	TotalBytesRecv int64 `json:"total_recv,string"`
@@ -65,6 +111,7 @@ type NetworkStats struct {
 	CurrentOutflow int64 `json:"current_outflow"`
 }
 
+// NetworkConnection models detailed information for one network connection.
 type NetworkConnection struct {
 	Incoming         bool             `json:"incoming"`
 	PeerID           string           `json:"peer_id"`
@@ -76,17 +123,20 @@ type NetworkConnection struct {
 	RemoteMetadata   NetworkMetadata  `json:"remote_metadata"`
 }
 
+// NetworkIDPoint models a point's address and port.
 type NetworkIDPoint struct {
 	Addr string `json:"addr"`
 	Port uint16 `json:"port"`
 }
 
+// NetworkVersion models a network-layer version of a node.
 type NetworkVersion struct {
 	Name  string `json:"name"`
 	Major uint16 `json:"major"`
 	Minor uint16 `json:"minor"`
 }
 
+// NetworkMetadata models metadata of a node.
 type NetworkMetadata struct {
 	DisableMempool bool `json:"disable_mempool"`
 	PrivateNode    bool `json:"private_node"`
--- network_test.go
+++ network_test.go
@@ -10,55 +10,71 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestGetNetworkStats(t *testing.T) {
-	expected := &NetworkStats{
-		TotalBytesSent: 291690080,
-		TotalBytesRecv: 532639553,
-		CurrentInflow:  23596,
-		CurrentOutflow: 14972,
+func TestServiceGetMethods(t *testing.T) {
+	ctx := context.Background()
+	tests := []struct {
+		get           func(s *Service) (interface{}, error)
+		fixture       string
+		expectedPath  string
+		expectedValue interface{}
+	}{
+		{
+			get:          func(s *Service) (interface{}, error) { return s.GetStats(ctx) },
+			fixture:      "fixtures/network/stat.json",
+			expectedPath: "/network/stat",
+			expectedValue: &NetworkStats{
+				TotalBytesSent: 291690080,
+				TotalBytesRecv: 532639553,
+				CurrentInflow:  23596,
+				CurrentOutflow: 14972,
+			},
+		},
+		{
+			get:           func(s *Service) (interface{}, error) { return s.GetConnections(ctx) },
+			fixture:       "fixtures/network/connections.json",
+			expectedPath:  "/network/connections",
+			expectedValue: []NetworkConnection{NetworkConnection{Incoming: false, PeerID: "idt5qvkLiJ15rb6yJU1bjpGmdyYnPJ", IDPoint: NetworkIDPoint{Addr: "::ffff:34.253.64.43", Port: 0x2604}, RemoteSocketPort: 0x2604, Versions: []NetworkVersion{NetworkVersion{Name: "TEZOS_ALPHANET_2018-07-31T16:22:39Z", Major: 0x0, Minor: 0x0}}, Private: false, LocalMetadata: NetworkMetadata{DisableMempool: false, PrivateNode: false}, RemoteMetadata: NetworkMetadata{DisableMempool: false, PrivateNode: false}}, NetworkConnection{Incoming: true, PeerID: "ids8VJTHEuyND6B8ahGgXPAJ3BDp1c", IDPoint: NetworkIDPoint{Addr: "::ffff:176.31.255.202", Port: 0x2604}, RemoteSocketPort: 0x2604, Versions: []NetworkVersion{NetworkVersion{Name: "TEZOS_ALPHANET_2018-07-31T16:22:39Z", Major: 0x0, Minor: 0x0}}, Private: true, LocalMetadata: NetworkMetadata{DisableMempool: true, PrivateNode: true}, RemoteMetadata: NetworkMetadata{DisableMempool: true, PrivateNode: true}}},
+		},
+		{
+			get: func(s *Service) (interface{}, error) {
+				return s.GetDelegateBalance(ctx, "main", "head", "tz3WXYtyDUNL91qfiCJtVUX746QpNv5i5ve5")
+			},
+			fixture:       "fixtures/contract/delegate_balance.json",
+			expectedPath:  "/chains/main/blocks/head/context/delegates/tz3WXYtyDUNL91qfiCJtVUX746QpNv5i5ve5/balance",
+			expectedValue: "13490453135591",
+		},
+		{
+			get: func(s *Service) (interface{}, error) {
+				return s.GetContractBalance(ctx, "main", "head", "tz3WXYtyDUNL91qfiCJtVUX746QpNv5i5ve5")
+			},
+			fixture:       "fixtures/contract/contract_balance.json",
+			expectedPath:  "/chains/main/blocks/head/context/contracts/tz3WXYtyDUNL91qfiCJtVUX746QpNv5i5ve5/balance",
+			expectedValue: "4700354460878",
+		},
 	}
 
-	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		require.Equal(t, "/network/stat", r.URL.Path)
-		require.Equal(t, http.MethodGet, r.Method)
+	for _, test := range tests {
+		srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			require.Equal(t, test.expectedPath, r.URL.Path)
+			require.Equal(t, http.MethodGet, r.Method)
 
-		buf, err := ioutil.ReadFile("fixtures/network/stat.json")
-		require.NoError(t, err, "error reading fixture")
+			buf, err := ioutil.ReadFile(test.fixture)
+			require.NoError(t, err, "error reading fixture %q", test.fixture)
 
-		_, err = w.Write(buf)
-		require.NoError(t, err, "error writing HTTP response")
-	}))
-	defer s.Close()
+			_, err = w.Write(buf)
+			require.NoError(t, err, "error writing HTTP response")
+		}))
 
-	c, err := NewClient(nil, s.URL)
-	require.NoError(t, err, "error creating client")
+		c, err := NewRPCClient(nil, srv.URL)
+		require.NoError(t, err, "error creating client")
 
-	stats, err := c.Network.GetStats(context.Background())
-	require.NoError(t, err, "error getting network stats")
+		s := &Service{client: c}
 
-	require.Equal(t, expected, stats, "unexpected stats response")
-}
+		value, err := test.get(s)
+		require.NoError(t, err, "error getting value")
 
-func TestGetNetworkConnections(t *testing.T) {
-	expected := []NetworkConnection{NetworkConnection{Incoming: false, PeerID: "idt5qvkLiJ15rb6yJU1bjpGmdyYnPJ", IDPoint: NetworkIDPoint{Addr: "::ffff:34.253.64.43", Port: 0x2604}, RemoteSocketPort: 0x2604, Versions: []NetworkVersion{NetworkVersion{Name: "TEZOS_ALPHANET_2018-07-31T16:22:39Z", Major: 0x0, Minor: 0x0}}, Private: false, LocalMetadata: NetworkMetadata{DisableMempool: false, PrivateNode: false}, RemoteMetadata: NetworkMetadata{DisableMempool: false, PrivateNode: false}}, NetworkConnection{Incoming: true, PeerID: "ids8VJTHEuyND6B8ahGgXPAJ3BDp1c", IDPoint: NetworkIDPoint{Addr: "::ffff:176.31.255.202", Port: 0x2604}, RemoteSocketPort: 0x2604, Versions: []NetworkVersion{NetworkVersion{Name: "TEZOS_ALPHANET_2018-07-31T16:22:39Z", Major: 0x0, Minor: 0x0}}, Private: true, LocalMetadata: NetworkMetadata{DisableMempool: true, PrivateNode: true}, RemoteMetadata: NetworkMetadata{DisableMempool: true, PrivateNode: true}}}
+		require.Equal(t, test.expectedValue, value, "unexpected value")
 
-	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		require.Equal(t, "/network/connections", r.URL.Path)
-		require.Equal(t, http.MethodGet, r.Method)
-
-		buf, err := ioutil.ReadFile("fixtures/network/connections.json")
-		require.NoError(t, err, "error reading fixture")
-
-		_, err = w.Write(buf)
-		require.NoError(t, err, "error writing HTTP response")
-	}))
-	defer s.Close()
-
-	c, err := NewClient(nil, s.URL)
-	require.NoError(t, err, "error creating client")
-
-	conns, err := c.Network.GetConnections(context.Background())
-	require.NoError(t, err, "error getting network connections")
-
-	require.Equal(t, expected, conns, "unexpected network connections response")
+		srv.Close()
+	}
 }
